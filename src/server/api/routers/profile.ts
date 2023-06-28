48ab0f8aae15c6173acc8105e1d3381dbfd23525
JT2M0L3Y <jtsmoley@icloud.com>
2023-06-28 11:10:44 -0700

    revalidate

--- src/server/api/routers/profile.ts
+++ src/server/api/routers/profile.ts
@@ -70,7 +70,8 @@ export const profileRouter = createTRPCRouter({
       }
 
       // revalidation
-      
+      void ctx.revalidateSSG?.(`/profiles/${userId}`)
+      void ctx.revalidateSSG?.(`/profiles/${currentUserId}`)
 
       return { addedFollow }
     })
